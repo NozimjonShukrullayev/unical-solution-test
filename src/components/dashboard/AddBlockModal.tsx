c1e73c1857a512b7b6522e66eebc43bb9e5ed724
Nozimjon <nozimshukrullayev@gmail.com>
2025-04-11 16:03:13 +0500

    Update AddBlockModal.tsx

--- src/components/dashboard/AddBlockModal.tsx
+++ src/components/dashboard/AddBlockModal.tsx
@@ -69,7 +69,7 @@ export const AddBlockModal = () => {
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
 					exit={{ opacity: 0 }}
-					className='fixed inset-0 z-[55] flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm'
+					className='fixed inset-0 z-[70] flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm'
 				>
 					<motion.div
 						initial={{ scale: 0.9, opacity: 0 }}
